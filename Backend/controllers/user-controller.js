c1b5749310547bc0100d193e8f9cfb2a39ed0931
Jarupat <jarupat.cho@student.mahidol.edu>
2025-05-06 15:09:34 +0700

    Log In & Sign Up

---
+++ Backend/controllers/user-controller.js
@@ -0,0 +1,125 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const { validatorResult } = require("express-validator");
+
+const User = require("../models/userModel");
+
+const HttpError = require("../models/http-errors");
+
+const login = async (req, res, next) => {
+    const { username, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ username: username });
+    } catch (err) {
+        const error = new HttpError(
+            "Login failed, please try again later.",
+            500
+        );
+        return next(error);
+    }
+
+    if (!existingUser) {
+        const error = new HttpError(
+            "Invalid credentials, could not log you in.",
+            403
+        );
+        return next(error);
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        const error = new HttpError(
+            "Could not log you in, please check your credentials and try again.",
+            500
+        );
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError(
+            "Invalid credentials, could not log you in.",
+            403
+        );
+        return next(error);
+    }
+
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, username: existingUser.username },
+            process.env.JWT_KEY,
+            { expiresIn: "1h" }
+        );
+        return res.json({
+            token,
+            message: "Logged in successfully",
+        });
+    } catch (err) {
+        const error = new HttpError(
+            "Login failed, please try again later.",
+            500
+        );
+        return next(error);
+    }
+};
+
+const signup = async (req, res, next) => {
+    const { username, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ username: username });
+    } catch (err) {
+        const error = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        );
+        return next(error);
+    }
+
+    if (existingUser) {
+        const error = new HttpError(
+            "User exists already, please login instead.",
+            422
+        );
+        return next(error);
+    }
+
+    let hashedPassword = await bcrypt.hash(String(password), 10);
+
+    const createdUser = new User({
+        username,
+        password: hashedPassword,
+    });
+
+    try {
+        await createdUser.save();
+    } catch (err) {
+        const error = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        );
+        return next(error);
+    }
+
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, username: createdUser.username },
+            process.env.JWT_KEY,
+            { expiresIn: "1h" }
+        );
+        return res.json({ token, message: "Signed up successfully" });
+    } catch (err) {
+        const error = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        );
+        return next(error);
+    }
+};
+
+exports.login = login;
+exports.signup = signup;
---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+require("dotenv").config();
+
+const userRoutes = require("./routes/user-routes");
+const mealRoutes = require("./routes/meal-routes");
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.header("Authorization");
+    if (!authHeader) {
+        return res
+            .status(401)
+            .json({ message: "Access denied. No token provided." });
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid token." });
+    }
+};
+
+app.use("/home", userRoutes);
+// app.use("/Meal", authenticateToken, mealRoutes);
+
+mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mealmatch.uo2sfna.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=MealMatch`
+).then(() => {
+    app.listen(8082);
+    console.log("Connected to MongoDB and server is running on port 8082");
+}).catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+});
